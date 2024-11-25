6dad370c4cd2cb4a161d386679724a6049e0ea9a
augustozanotto <augustozanotto@outlook.it>
2024-11-25 18:59:49 +0100

    NON VALIDO

--- my-learning/src/app/layout.js
+++ my-learning/src/app/layout.js
@@ -1,7 +1,10 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* Layout UI */}
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
